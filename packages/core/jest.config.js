ef6c65915bd2ed16400d2432e99900a8ce1fb60c
lukeking <countstarss@163.com>
2025-03-30 12:18:55 +0800

    chore: 修改 core 的jest测试配置

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,8 +1,15 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/__tests__'],
+  moduleFileExtensions: ['ts', 'js'],
   transform: {
-    '^.+\.tsx?$': ['ts-jest', {}],
+    '^.+\\.tsx?$': ['ts-jest', {}],
   },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
 };
+
+module.exports = config;
